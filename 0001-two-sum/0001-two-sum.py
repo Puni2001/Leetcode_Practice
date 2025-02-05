4e6cb4b7cba1510a38babb9cd8ccc51c46b7699d
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-02-05 21:34:10 +0530

    Time: 0 ms (100%), Space: 18.8 MB (43.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,14 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
 
-        for i , val in enumerate(nums):
+        for i,val in enumerate(nums):
             complement = target - val
             if complement in hashmap:
                 return [hashmap[complement],i]
 
             else:
                 hashmap[val] = i
-
-
-
-                
