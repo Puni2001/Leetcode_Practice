9f90c469cdc10129763abba390ef1af5837a7fb1
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-02-10 16:20:54 +0530

    Time: 12 ms (54.12%), Space: 17.9 MB (46.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        """
+        check the len of s == t if not return false 
+        count = [0] * 26 
+        1+ if char in s 
+        1- if char in t 
 
+        finally we check all count is 0 or not is not then it is not anagram 
+        
+        """
+        if len(s) != len(t):
+            return False 
+
+        
         count = [0] * 26
 
         for char_s , char_t in zip(s,t):
-            count[ord(char_s) - ord('a')] += 1
-            count[ord(char_t) - ord('a')] -= 1
-
+            count[ord(char_s)-ord('a')] += 1
+            count[ord(char_t)-ord('a')] -= 1
 
         return all(x==0 for x in count)
-
-        
