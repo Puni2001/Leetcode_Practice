5514ef303f2a671bd31569e4e75499624d3f65c3
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-02-12 07:28:04 +0530

    Time: 0 ms (100%), Space: 17.9 MB (61.5%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []  # This will store all the subsets we generate.
+        result = []
 
-        def backtrack(start, path):
-            # Each time we enter backtrack, we add the current path (subset) to result.
-            result.append(path[:])  # Make a copy of the current subset and add it.
+        def backtrack(index, current_subset):
+            if index == len(nums):
+                result.append(current_subset[:])
+                return
+            
+            # include nums[index]
+            current_subset.append(nums[index])
+            backtrack(index+1,current_subset)
 
-            # Now, we loop through all possible next elements starting from 'start'
-            for i in range(start, len(nums)):
-                path.append(nums[i])       # Choose nums[i] to be in the current subset.
-                backtrack(i + 1, path)     # Recursively explore further subsets with nums[i] included.
-                path.pop()                 # Remove the last added element to backtrack (undo the choice).
 
-        backtrack(0, [])  # Start from index 0 with an empty subset.
+            # backtrack or exclude nums[index]
+            current_subset.pop()
+            backtrack(index+1, current_subset)
+
+
+        backtrack(0,[])
         return result
