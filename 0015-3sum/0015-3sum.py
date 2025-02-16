f85aee91413de0cc9faef4a4a18dd719c9b9eda2
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-02-16 11:47:41 +0530

    Time: 636 ms (35.84%), Space: 20.7 MB (39.12%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,41 +1,31 @@
 class Solution:
-   # def threeSum(self, nums: List[int]) -> List[List[int]]:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # [-1,0,1,2,-1,-4]
-
-        # [-4,-1,-1,0,1,2]
+      #  result = []
         nums.sort()
-        res = []
+        result = []
         for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]: # skip duplicates 
                 continue
 
-            
-            
-            l = i+1
-            r = len(nums)-1
-            while l < r:
-                total = nums[i] + nums[l] + nums[r]
+            left , right = i + 1 , len(nums) - 1
+            while left < right:
+                current_sum = nums[i] +nums[left]+nums[right]
+                if current_sum == 0:
+                    result.append([nums[i],nums[left],nums[right]])
+                    left += 1
+                    right -= 1
 
-                if total == 0:
-                    res.append([nums[i],nums[l],nums[r]])
-                    l +=1
-                    r -=1
+                    # check for duplicates 
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
 
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
 
-                    while l < r and nums[r] == nums[r+1]:
-                        r -= 1
-
-
-                
-                elif total > 0:
-                    r -= 1
-                
+                elif current_sum < 0:
+                    left += 1
                 else:
-                    l += 1
+                    right -= 1
 
-        return res
-
-
+        return result
+            
