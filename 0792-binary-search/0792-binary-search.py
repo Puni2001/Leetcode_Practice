6e2ba2744484825094686d8e38248b8024c102a5
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-02-16 12:10:52 +0530

    Time: 0 ms (100%), Space: 18.8 MB (45.22%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,15 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums)-1
+        """
+        nums should be sorted 
+        use 2 ptr left = 0 , right = len(nums) - 1
+        mid = ( left + right ) // 2
+        loop through nums 
+        if mid == target 
+            return its index
+        elif mid < target 
+            left mid + 1  # if its less than target no points to check half we can move from next element 
+        else 
+            right mid - 1  # if its greater than target right to next elemt 
+               
+        """
+        left , right = 0 , len(nums)-1
+        while left <= right:
+            mid = ( left + right ) // 2
 
-        while l <= r:
-            mid = (l+r) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
-                r = mid - 1
-
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                l = mid + 1
+                right = mid - 1
+
         return -1
