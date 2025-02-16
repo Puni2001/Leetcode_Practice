bac45ada6c6088c5655934bfbcf3eb024af0bd84
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-02-16 09:40:15 +0530

    Time: 587 ms (54.99%), Space: 20.6 MB (69.06%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,41 @@
 class Solution:
+   # def threeSum(self, nums: List[int]) -> List[List[int]]:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        result = []
+        # [-1,0,1,2,-1,-4]
 
-        for i in range(len(nums)-2):
+        # [-4,-1,-1,0,1,2]
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            left , right = i + 1 , len(nums)-1
+            
+            
+            l = i+1
+            r = len(nums)-1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
 
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    res.append([nums[i],nums[l],nums[r]])
+                    l +=1
+                    r -=1
 
-                if current_sum == 0:
-                    result.append([nums[i], nums[left], nums[right]])
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
 
-                    while left < right and nums[left] == nums[left + 1]:
-                        left +=1 
-
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
-
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
 
 
-                    left += 1 
-                    right -= 1
+                
+                elif total > 0:
+                    r -= 1
+                
+                else:
+                    l += 1
 
-                elif current_sum < 0:
-                    left += 1
+        return res
 
-                else: 
-                    right -= 1
-
-        return result
 
