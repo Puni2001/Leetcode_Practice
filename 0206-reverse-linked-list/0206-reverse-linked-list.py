9d9cc65ed20d74c5a9b23b866caf5ae6a0537116
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-03-07 11:54:39 +0530

    Time: 0 ms (100%), Space: 18.5 MB (95.82%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,28 +6,23 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         """
-        h
-        1 -> 2 -> 3 -> 4 -> 5
-prev   curr
-        prev = None 
-        head = curr 
+         h
+         1 -> 2 -> 3 -> 4 -> 5
+  prev  curr next_node 
+
+        ponites prev to None and curr to head 
+        store the next_node 
+        curr.next connect prev 
+        move prev to curr
+        curr to next node 
         
-        traverse the linked list untill head is None 
-        next_node = curr.next
-        curr.next = prev
-        prev = curr
-        curr = next_node 
-
-        return prev 
-
         """
-
-        prev = None
+        prev = None 
         curr = head 
         while curr:
-          next_node = curr.next 
-          curr.next = prev 
-          prev = curr 
-          curr = next_node 
+          next_node = curr.next
+          curr.next = prev
+          prev = curr
+          curr = next_node
 
         return prev 
