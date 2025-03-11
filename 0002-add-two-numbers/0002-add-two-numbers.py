b0e071e0ea03f52be857465c444decfc5f5fb175
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-03-11 10:17:40 +0530

    Time: 7 ms (32.57%), Space: 17.8 MB (57.47%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,23 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, list1, list2):
-        l1 = list1
-        l2 = list2
-        cry = 0
-        result = ListNode(0)
-        temp = result
-        
-        while l1 is not None or l2 is not None:
-            if l1 is not None:
-                cry += l1.val
-                l1 = l1.next
-            if l2 is not None:
-                cry += l2.val
-                l2 = l2.next
-            temp.next = ListNode(cry % 10)
-            temp = temp.next
-            cry = cry // 10
-        
-        if cry == 1:
-            temp.next = ListNode(cry)
-        
-        return result.next
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            sum = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            carry = sum // 10
+            curr.next = ListNode(sum % 10)
+            curr = curr.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
