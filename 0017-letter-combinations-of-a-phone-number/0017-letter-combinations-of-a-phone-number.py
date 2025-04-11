6e2b18593f5b824cafe474a08d2b25d1b1d2785b
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-04-11 13:22:08 +0530

    Time: 0 ms (100%), Space: 18 MB (23.16%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+          if not digits:
+            return []
+
+          digit_to_char = {
+              "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+              "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+          }
+
+          result = []
+
+          def backtrack(index, path):
+              if len(path) == len(digits):
+                  result.append(path)
+                  return
+
+              for char in digit_to_char[digits[index]]:
+                  backtrack(index + 1, path + char)
+
+          if digits:
+            backtrack(0, "")
+          return result
