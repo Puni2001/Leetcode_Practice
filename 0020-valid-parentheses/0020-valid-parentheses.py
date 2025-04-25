688c3025607422a59c84e21dcdf084a4b65bab0c
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-04-25 19:17:15 +0530

    Time: 4 ms (9.19%), Space: 17.9 MB (51.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # Initialize an empty stack and a hash map for matching brackets
+        closedToOpen = {")":"(","}":"{","]":"["}
         stack = []
-        hash = {')': '(', ']': '[', '}': '{'}
-        
-        # Loop through each character in the string
-        for char in s:
-            if char in hash:
-                # If it's a closing bracket, check the stack
-                if stack and stack[-1] == hash[char]:
-                    stack.pop()  # Remove the matching opening bracket
-                else:
-                    return False  # Invalid if no match
+
+        for c in s:
+          if c in closedToOpen:
+            if stack and stack[-1] == closedToOpen[c]:
+              stack.pop()
             else:
-                # Push opening brackets onto the stack
-                stack.append(char)
-        
-        # Return True if stack is empty, False otherwise
+              return False 
+
+          else:
+            stack.append(c)
+
         return not stack
