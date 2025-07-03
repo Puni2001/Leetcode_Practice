65cebe0d9043424920080345b16260080ed19b01
Punith S <111702110+Puni2001@users.noreply.github.com>
2025-07-03 10:01:59 +0530

    Time: 2 ms (98.46%), Space: 42.9 MB (90.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,26 +4,15 @@ class Solution {
         int right = s.length()-1;
 
         while (left < right){
-          while (left < right && !alphaNum(s.charAt(left))){
-            left ++;
-          }
-          while(left < right && !alphaNum(s.charAt(right))){
-            right--;
-          }
+          while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+          while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+
           if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
             return false;
           }
           left++;
           right--;
-
         }
         return true;
     }
-
-    public boolean alphaNum(char c){
-      return (c >='A' && c <= 'Z' ||
-      c >= 'a' && c <= 'z' ||
-      c >= '0' && c <= '9');
-    }
-
 }
